53785cf737aabbd9baec3ab864be3f9be21c3ad5
cerna <cernamaerompe@gmail.com>
2025-03-16 00:06:08 +0800

    Initial commit - Bootstrap site with Express.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
